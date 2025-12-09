6a5c4927abb5d15b2a35a9f50c4162c2ef6ae0b7
Karthik Sajwan <karthiksajwan03@gmail.com>
2025-12-10 00:12:29 +0530

    Updated FastAPI routes and code

--- app/app.py
+++ app/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
-
+from app.schemas import PostCreate
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, engine
+from app import models
 app = FastAPI()
 
-text_posts = {}
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/posts")
 def get_all_post():
@@ -14,3 +26,9 @@ def get_post(id: int):
         raise HTTPException(status_code=404, detail = "Post not found")
     
     return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post: PostCreate):
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
